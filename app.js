915b77769616e365f0dec92959daf64b0a4cc6e4
Kevin Satur <kevin@edapt.nz>
2020-06-11 19:33:23 +1200

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.post('/shipping', function(req, res){
+	res.json({
+		"rates": [{
+			"service_name": "Kevin's awesome rate",
+			"description": "Let's make it free",
+			"service_code": "kevin_rate",
+			"currency": "NZD",
+			"total_price": "0000"
+		}]	
+	});
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, ()=>console.log(`Listening on Port: ${port}`));
